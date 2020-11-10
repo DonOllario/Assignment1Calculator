368281aa5527e3d93ed594dd03044d7cc7c251db
DonOllario <oliver.stenstrom@hotmail.com>
2020-11-10 20:51:57 +0100

    First Commit

---
+++ InlämningsUppgift1CalculatorGit/LvlThreeOperatorCalculator.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InlämningsUppgift1CalculatorGit
+{
+    class LvlThreeOperatorCalculator
+    {
+        char Operator1, Operator2;
+        int Term1, Term2, Term3;
+
+        public void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.BufferWidth));
+            Console.Write(new string(' ', Console.BufferWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+        public void FirstInput()
+        {
+            Console.WriteLine("Enter first operator.");
+            Operator1 = Convert.ToChar(Console.ReadLine());
+            Console.SetCursorPosition(0, Console.CursorTop - 2);
+            ClearCurrentConsoleLine();
+            Console.WriteLine("Enter second operator.");
+            Operator2 = Convert.ToChar(Console.ReadLine());
+            Console.SetCursorPosition(0, Console.CursorTop - 2);
+            ClearCurrentConsoleLine();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter first term.");
+                    Term1 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(0, Console.CursorTop - 2);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine("Enter second term. ");
+                    Term2 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(0, Console.CursorTop - 2);
+                    ClearCurrentConsoleLine();
+                    Console.WriteLine("Enter third term. ");
+                    Term3 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(0, Console.CursorTop - 2);
+                    ClearCurrentConsoleLine();
+                    break;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("<Number was invalid. Press enter and enter a valid number.>");
+                    Console.ReadLine();
+                    Console.SetCursorPosition(0, Console.CursorTop - 3);
+                    ClearCurrentConsoleLine();
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    ClearCurrentConsoleLine();
+                    continue;
+                }
+            }
+        }
+
+        public string HandleOperator()
+        {
+            int result;
+
+            if (Operator2 == '*')
+            {
+                result = Term2 * Term3;
+                if (Operator1 == '*')
+                {
+                    result = Term1 * result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '/')
+                {
+                    result = Term1 / result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '+')
+                {
+                    result = Term1 + result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '-')
+                {
+                    result = Term1 - result;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else if (Operator2 == '/')
+            {
+                result = Term2 / Term3;
+                if (Operator1 == '*')
+                {
+                    result = Term1 * result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '/')
+                {
+                    result = Term1 / result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '+')
+                {
+                    result = Term1 + result;
+                    return Convert.ToString(result);
+                }
+                else if (Operator1 == '-')
+                {
+                    result = Term1 - result;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else if (Operator1 == '*')
+            {
+                result = Term1 * Term2;
+                if (Operator2 == '*')
+                {
+                    result *= Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '/')
+                {
+                    result /= Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '+')
+                {
+                    result += Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '-')
+                {
+                    result -= Term3;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else if (Operator1 == '/')
+            {
+                result = Term1 / Term2;
+                if (Operator2 == '*')
+                {
+                    result *= Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '/')
+                {
+                    result /= Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '+')
+                {
+                    result += Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '-')
+                {
+                    result -= Term3;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else if (Operator1 == '+')
+            {
+                result = Term1 + Term2;
+                if (Operator2 == '+')
+                {
+                    result += Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '-')
+                {
+                    result -= Term3;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else if (Operator1 == '-')
+            {
+                result = Term1 - Term2;
+                if (Operator2 == '+')
+                {
+                    result += Term3;
+                    return Convert.ToString(result);
+                }
+                else if (Operator2 == '-')
+                {
+                    result -= Term3;
+                    return Convert.ToString(result);
+                }
+                else
+                {
+                    return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+                }
+            }
+            else
+            {
+                return "Något gick snett. Var säker på att du skriver in korrekta operatorer och termer.";
+            }
+        }
+    }
+}
---
+++ InlämningsUppgift1CalculatorGit/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InlämningsUppgift1CalculatorGit
+{
+    class Program
+    {
+        static void ClearCurrentConsoleLine()
+        {
+            int currentLineCursor = Console.CursorTop;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.BufferWidth));
+            Console.Write(new string(' ', Console.BufferWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }
+
+        static void Main(string[] args)
+        {
+            LvlThreeOperatorCalculator MyCalc = new LvlThreeOperatorCalculator();
+            Console.WriteLine("---------------LvlThreeOperatorCalculator---------------");
+            while (true)
+            {
+                MyCalc.FirstInput();
+                Console.Write("Resultatet är: ");
+                Console.Write(MyCalc.HandleOperator());
+                Console.WriteLine("\nTryck Enter för att fortsätta, vill du avsluta skriv stop");
+                
+                string stop = Console.ReadLine();
+                if (stop == "stop")
+                {
+                    Environment.Exit(0);
+                }
+                Console.SetCursorPosition(0, Console.CursorTop - 3);
+                ClearCurrentConsoleLine();
+            }
+        }
+    }
+}
